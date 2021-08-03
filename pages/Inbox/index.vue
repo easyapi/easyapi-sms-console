f8b86a07fb22fdd99000f3509fa8d8318fa48a74
shdboom <928421021@qq.com>
2021-08-03 11:57:32 +0800

    表格数据处理问题

--- pages/Inbox/index.vue
+++ pages/Inbox/index.vue
@@ -12,6 +12,7 @@
         <el-divider></el-divider>
         <div class='main-content'>
           <el-table
+            v-loading="loading"
             :data='smsReceiveList'
             style='width: 100%'>
             <el-table-column
@@ -38,9 +39,12 @@
               prop='address'
               label='内容'>
             </el-table-column>
+            <template slot="empty" style="height:200px;line-height: 200px;">
+              <el-empty :image-size="200"></el-empty>
+            </template>
           </el-table>
         </div>
-        <Pagination @fatherSize='fatherSize' @fatherCurrent='fatherCurrent' :size='pagination.size'
+        <Pagination v-if="isShow" @fatherSize='fatherSize' @fatherCurrent='fatherCurrent' :size='pagination.size'
                     :total-elements='pagination.total' class='paging'></Pagination>
         <div style='clear: both'></div>
       </div>
@@ -50,126 +54,131 @@
 </template>
 
 <script>
-import Header from '../../components/Header/index.vue'
-import Aside from '../../components/Aside/index.vue'
-import Pagination from '../../components/Pagination/index'
-import { getSmsReceiveList } from '../../api/sms-receive'
+  import Header from '../../components/Header/index.vue'
+  import Aside from '../../components/Aside/index.vue'
+  import Pagination from '../../components/Pagination/index'
+  import {getSmsReceiveList} from '../../api/sms-receive'
 
-export default {
-  name: '',
-  components: {
-    Header,
-    Aside,
-    Pagination
-  },
-  data() {
-    return {
-      smsReceiveList: [],
-      title: '',
-      showHeader: '',
-      pagination: {
-        page: 1,
-        size: 12,
-        total: 0
-      }
-    }
-  },
-  head() {
-    return {
-      title: '金融专辑 - EasyAPI服务市场',
-      meta: [
-        { hid: 'description', name: 'description', content: '服务市场场景化服务' },
-        { hid: 'keyword', name: 'keyword', content: '服务市场场景化服务' }
-      ]
-    }
-  },
-  methods: {
-    /**
-     * 获取短信收件箱列表
-     */
-    getSmsReceiveList() {
-      let page = this.pagination.page - 1
-      let params = {
-        sort: 'addTime,desc',
-        page: page,
-        size: this.pagination.size
-      }
-      getSmsReceiveList(params, this).then(res => {
-        if (res.data.code === 1) {
-          this.smsReceiveList = res.data.content
-          this.pagination.total = Number(res.data.totalElements)
-        } else {
-          this.smsReceiveList = []
-          this.pagination.total = 0
+  export default {
+    name: '',
+    components: {
+      Header,
+      Aside,
+      Pagination
+    },
+    data() {
+      return {
+        smsReceiveList: [],
+        loading: true,
+        isShow: true,
+        title: '',
+        showHeader: '',
+        pagination: {
+          page: 1,
+          size: 12,
+          total: 0
         }
-      })
+      }
     },
-    /**
-     * 添加文章
-     */
-    createArticle() {
-      this.$refs.editArticle.dialogVisible = true
-      this.$refs.editArticle.title = '添加文章'
-      this.$refs.editArticle.articleForm = this.$options.data()
+    head() {
+      return {
+        title: '金融专辑 - EasyAPI服务市场',
+        meta: [
+          {hid: 'description', name: 'description', content: '服务市场场景化服务'},
+          {hid: 'keyword', name: 'keyword', content: '服务市场场景化服务'}
+        ]
+      }
     },
-    /**
-     * 修改文章
-     */
-    updateArticle(row) {
-      this.$refs.editArticle.dialogVisible = true
-      this.$refs.editArticle.title = '编辑文章'
-      this.$refs.editArticle.articleForm = row
-      this.$refs.editArticle.articleId = row.articleId
-      this.$refs.editArticle.articleForm.articleCategoryId = row.articleCategory.articleCategoryId
-    },
-    //删除文章
-    deleteArticle(row) {
-      this.$confirm('您确定要删除该文章吗?', '提示', {
-        confirmButtonText: '确定',
-        cancelButtonText: '取消',
-        type: 'warning'
-      }).then(() => {
-        deleteArticle(row.articleId, this).then(res => {
+    methods: {
+      /**
+       * 获取短信收件箱列表
+       */
+      getSmsReceiveList() {
+        let page = this.pagination.page - 1
+        let params = {
+          sort: 'addTime,desc',
+          page: page,
+          size: this.pagination.size
+        }
+        getSmsReceiveList(params, this).then(res => {
           if (res.data.code === 1) {
-            this.getSmsReceiveList()
-            this.$message({
-              type: 'success',
-              message: '删除成功!'
-            })
+            this.loading = false
+            this.smsReceiveList = res.data.content
+            this.pagination.total = Number(res.data.totalElements)
+          } else {
+            this.loading = false
+            this.smsReceiveList = []
+            this.isShow = false
+            this.pagination.total = 0
           }
         })
-      })
-    },
-    //分页
-    fatherSize(data) {
-      this.pagination.size = data
-      this.getSmsReceiveList()
-    },
-    fatherCurrent(data) {
-      this.pagination.page = data
-      this.getSmsReceiveList()
-    },
-    search(item) {
-      console.log(1111, item)
-      let { title } = item
-      this.title = title
-      this.getSmsReceiveList()
-    },
-    reset(item) {
-      console.log(1111, item)
+      },
+      /**
+       * 添加文章
+       */
+      createArticle() {
+        this.$refs.editArticle.dialogVisible = true
+        this.$refs.editArticle.title = '添加文章'
+        this.$refs.editArticle.articleForm = this.$options.data()
+      },
+      /**
+       * 修改文章
+       */
+      updateArticle(row) {
+        this.$refs.editArticle.dialogVisible = true
+        this.$refs.editArticle.title = '编辑文章'
+        this.$refs.editArticle.articleForm = row
+        this.$refs.editArticle.articleId = row.articleId
+        this.$refs.editArticle.articleForm.articleCategoryId = row.articleCategory.articleCategoryId
+      },
+      //删除文章
+      deleteArticle(row) {
+        this.$confirm('您确定要删除该文章吗?', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+          deleteArticle(row.articleId, this).then(res => {
+            if (res.data.code === 1) {
+              this.getSmsReceiveList()
+              this.$message({
+                type: 'success',
+                message: '删除成功!'
+              })
+            }
+          })
+        })
+      },
+      //分页
+      fatherSize(data) {
+        this.pagination.size = data
+        this.getSmsReceiveList()
+      },
+      fatherCurrent(data) {
+        this.pagination.page = data
+        this.getSmsReceiveList()
+      },
+      search(item) {
+        console.log(1111, item)
+        let {title} = item
+        this.title = title
+        this.getSmsReceiveList()
+      },
+      reset(item) {
+        console.log(1111, item)
 
+      },
+      event(item) {
+        let {title} = item
+        this.title = title
+      }
     },
-    event(item) {
-      let { title } = item
-      this.title = title
+    mounted() {
+      this.getSmsReceiveList()
+      // this.getMonthChart()
+      this.showHeader = this.theme.showHeader
     }
-  },
-  mounted() {
-    this.getSmsReceiveList()
-    // this.getMonthChart()
-    this.showHeader = this.theme.showHeader
   }
-}
 </script>
 
 <style lang='scss'>
--- pages/billing/index.vue
+++ pages/billing/index.vue
@@ -12,6 +12,7 @@
         <el-divider></el-divider>
         <div class='main-content'>
           <el-table
+            v-loading="loading"
             :data='smsSummayList'
             style='width: 100%'>
             <el-table-column
@@ -38,9 +39,12 @@
               prop='content'
               label='内容'>
             </el-table-column>
+            <template slot="empty" style="height:200px;line-height: 200px;">
+              <el-empty :image-size="200"></el-empty>
+            </template>
           </el-table>
         </div>
-        <Pagination @fatherSize='fatherSize' @fatherCurrent='fatherCurrent' :size='pagination.size'
+        <Pagination v-if="isShow" @fatherSize='fatherSize' @fatherCurrent='fatherCurrent' :size='pagination.size'
                     :total-elements='pagination.total' class='paging'></Pagination>
         <div style='clear: both'></div>
       </div>
@@ -50,107 +54,112 @@
 </template>
 
 <script>
-import Header from '../../components/Header/index.vue'
-import Aside from '../../components/Aside/index.vue'
-import Pagination from '../../components/Pagination/index'
-import { getSmsRecordList } from '../../api/sms-record'
+  import Header from '../../components/Header/index.vue'
+  import Aside from '../../components/Aside/index.vue'
+  import Pagination from '../../components/Pagination/index'
+  import {getSmsRecordList} from '../../api/sms-record'
 
-export default {
-  name: '',
-  components: {
-    Header,
-    Aside,
-    Pagination
-  },
-  data() {
-    return {
-      smsSummayList: [],
-      title: '',
-      showHeader: '',
-      pagination: {
-        page: 1,
-        size: 12,
-        total: 0
-      }
-    }
-  },
-  head() {
-    return {
-      title: '金融专辑 - EasyAPI服务市场',
-      meta: [
-        { hid: 'description', name: 'description', content: '服务市场场景化服务' },
-        { hid: 'keyword', name: 'keyword', content: '服务市场场景化服务' }
-      ]
-    }
-  },
-  methods: {
-    /**
-     * 获取短信收件箱列表
-     */
-    getSmsRecordList() {
-      let page = this.pagination.page - 1
-      let params = {
-        sort: 'addTime,desc',
-        page: page,
-        size: this.pagination.size
-      }
-      getSmsRecordList(params, this).then(res => {
-        if (res.data.code === 1) {
-          this.smsSummayList = res.data.content
-          this.pagination.total = Number(res.data.totalElements)
-        } else {
-          this.smsSummayList = []
-          this.pagination.total = 0
+  export default {
+    name: '',
+    components: {
+      Header,
+      Aside,
+      Pagination
+    },
+    data() {
+      return {
+        smsSummayList: [],
+        loading: true,
+        isShow: true,
+        title: '',
+        showHeader: '',
+        pagination: {
+          page: 1,
+          size: 12,
+          total: 0
         }
-      })
+      }
     },
-    /**
-     * 添加文章
-     */
-    createArticle() {
-      this.$refs.editArticle.dialogVisible = true
-      this.$refs.editArticle.title = '添加文章'
-      this.$refs.editArticle.articleForm = this.$options.data()
+    head() {
+      return {
+        title: '金融专辑 - EasyAPI服务市场',
+        meta: [
+          {hid: 'description', name: 'description', content: '服务市场场景化服务'},
+          {hid: 'keyword', name: 'keyword', content: '服务市场场景化服务'}
+        ]
+      }
     },
-    /**
-     * 修改文章
-     */
-    updateArticle(row) {
-      this.$refs.editArticle.dialogVisible = true
-      this.$refs.editArticle.title = '编辑文章'
-      this.$refs.editArticle.articleForm = row
-      this.$refs.editArticle.articleId = row.articleId
-      this.$refs.editArticle.articleForm.articleCategoryId = row.articleCategory.articleCategoryId
-    },
-    //分页
-    fatherSize(data) {
-      this.pagination.size = data
-      this.getSmsRecordList()
-    },
-    fatherCurrent(data) {
-      this.pagination.page = data
-      this.getSmsRecordList()
-    },
-    search(item) {
-      console.log(1111, item)
-      let { title } = item
-      this.title = title
-      this.getSmsRecordList()
-    },
-    reset(item) {
-      console.log(1111, item)
+    methods: {
+      /**
+       * 获取短信收件箱列表
+       */
+      getSmsRecordList() {
+        let page = this.pagination.page - 1
+        let params = {
+          sort: 'addTime,desc',
+          page: page,
+          size: this.pagination.size
+        }
+        getSmsRecordList(params, this).then(res => {
+          if (res.data.code === 1) {
+            this.loading = false
+            this.smsSummayList = res.data.content
+            this.pagination.total = Number(res.data.totalElements)
+          } else {
+            this.loading = false
+            this.smsSummayList = []
+            this.isShow = false
+            this.pagination.total = 0
+          }
+        })
+      },
+      /**
+       * 添加文章
+       */
+      createArticle() {
+        this.$refs.editArticle.dialogVisible = true
+        this.$refs.editArticle.title = '添加文章'
+        this.$refs.editArticle.articleForm = this.$options.data()
+      },
+      /**
+       * 修改文章
+       */
+      updateArticle(row) {
+        this.$refs.editArticle.dialogVisible = true
+        this.$refs.editArticle.title = '编辑文章'
+        this.$refs.editArticle.articleForm = row
+        this.$refs.editArticle.articleId = row.articleId
+        this.$refs.editArticle.articleForm.articleCategoryId = row.articleCategory.articleCategoryId
+      },
+      //分页
+      fatherSize(data) {
+        this.pagination.size = data
+        this.getSmsRecordList()
+      },
+      fatherCurrent(data) {
+        this.pagination.page = data
+        this.getSmsRecordList()
+      },
+      search(item) {
+        console.log(1111, item)
+        let {title} = item
+        this.title = title
+        this.getSmsRecordList()
+      },
+      reset(item) {
+        console.log(1111, item)
 
+      },
+      event(item) {
+        let {title} = item
+        this.title = title
+      }
     },
-    event(item) {
-      let { title } = item
-      this.title = title
+    mounted() {
+      this.getSmsRecordList()
+      this.showHeader = this.theme.showHeader
     }
-  },
-  mounted() {
-    this.getSmsRecordList()
-    this.showHeader = this.theme.showHeader
   }
-}
 </script>
 
 <style lang='scss'>
