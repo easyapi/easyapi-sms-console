c1bdd568ef349fe0483ab6c33cee338056469ba9
chenyun123 <1537299563@qq.com>
2021-08-03 18:49:06 +0800

    axios请求修改

--- plugins/axios.js
+++ plugins/axios.js
@@ -12,11 +12,11 @@ export default function ({$axios, redirect}) {
     const code = parseInt(error.response && error.response.status)
     if (code === 400) {
       if (error.response.data.code === -9) {
-        // window.location.href =
-        //   'https://account.easyapi.com/login?from=https://sms.easyapi.com'
+        window.location.href =
+          'https://account.easyapi.com/login?from=https://sms.easyapi.com'
       } else if (error.response.data.code === -8) {
-        // window.location.href =
-        //   'https://team.easyapi.com/new?from=https://sms.easyapi.com'
+        window.location.href =
+          'https://team.easyapi.com/new?from=https://sms.easyapi.com'
       } else {
         Message.error(error.response.data.message)
       }
