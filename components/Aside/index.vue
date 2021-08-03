02a82bbec83d5b66c6daa74a0873f12f2a8ecaf5
shdboom <928421021@qq.com>
2021-08-03 17:45:34 +0800

    三级菜单栏

--- components/Aside/index.vue
+++ components/Aside/index.vue
@@ -14,23 +14,26 @@
           </nuxt-link>
         </div>
 
-        <!--<div class="menu" v-if="menuType == 2">-->
-        <!--<el-menu>-->
-        <!--<el-menu-item v-if="!item.children" index="2">-->
-        <!--<i :class="item.icon"></i>-->
-        <!--<span slot="title">{{item.title}}</span>-->
-        <!--</el-menu-item>-->
-        <!--<el-submenu v-if="item.children">-->
-        <!--<template slot="title">-->
-        <!--<i :class="item.icon"></i>-->
-        <!--<span>{{item.title}}</span>-->
-        <!--</template>-->
-        <!--<el-menu-item-group>-->
-        <!--<el-menu-item v-for="(a, index) in item.children">{{a.title}}</el-menu-item>-->
-        <!--</el-menu-item-group>-->
-        <!--</el-submenu>-->
-        <!--</el-menu>-->
-        <!--</div>-->
+        <div v-if="menuType == 2">
+          <el-menu
+            default-active="2"
+            class="el-menu-vertical-demo"
+            @select="handleSelectMenu">
+            <el-menu-item :index="item.path" v-if="item.children.length==0">
+              <i :class="item.icon"></i>
+              <span>{{item.title}}</span>
+            </el-menu-item>
+            <el-submenu v-if="item.children.length>0">
+              <template slot="title">
+                <i :class="item.icon"></i>
+                <span>{{item.title}}</span>
+              </template>
+              <el-menu-item-group>
+                <el-menu-item :index="a.path" v-for="a in item.children">{{a.title}}</el-menu-item>
+              </el-menu-item-group>
+            </el-submenu>
+          </el-menu>
+        </div>
 
 
       </div>
@@ -53,6 +56,7 @@
                 title: '短信概览',
                 path: '/overview',
                 icon: 'el-icon-s-help',
+                children: []
               },
             ],
           },
@@ -63,6 +67,7 @@
                 title: '短信模板',
                 path: '/template',
                 icon: 'el-icon-s-help',
+                children: []
               },
             ],
           },
@@ -73,21 +78,41 @@
                 title: '短信收件箱',
                 path: '/Inbox',
                 icon: 'el-icon-s-help',
+                children: []
               },
               {
                 title: '短信发送',
                 path: '/send',
                 icon: 'el-icon-s-help',
+                children: []
               },
               {
                 title: '计费日志',
                 path: '/billing',
                 icon: 'el-icon-s-help',
+                children: [
+                  {
+                    title: '测试1',
+                    path: '/123'
+                  },
+                  {
+                    title: '测试2',
+                    path: '/456'
+                  },
+                ]
               }
             ],
           },
         ],
       }
+    },
+    methods: {
+      handleSelectMenu(key, keyPath) {
+        console.log(key);
+        console.log(keyPath);
+        let path = keyPath[0];
+        this.$router.push({path: path});
+      },
     },
     mounted() {
       this.activePath = this.$route.path ? `${this.$route.path}` : '/overview'
--- plugins/axios.js
+++ plugins/axios.js
@@ -1,8 +1,9 @@
 import Cookies from 'js-cookie'
-import { Message } from 'element-ui'
-export default function ({ $axios, redirect }) {
+import {Message} from 'element-ui'
+
+export default function ({$axios, redirect}) {
   $axios.onRequest((config) => {
-    let token = Cookies.get('Authorization')
+    let token = Cookies.get('authenticationToken')
     if (token) {
       config.headers.Authorization = 'Bearer ' + token
     }
@@ -11,11 +12,11 @@ export default function ({ $axios, redirect }) {
     const code = parseInt(error.response && error.response.status)
     if (code === 400) {
       if (error.response.data.code === -9) {
-        window.location.href =
-          'https://account.easyapi.com/login?from=https://sms.easyapi.com'
+        // window.location.href =
+        //   'https://account.easyapi.com/login?from=https://sms.easyapi.com'
       } else if (error.response.data.code === -8) {
-        window.location.href =
-          'https://team.easyapi.com/new?from=https://sms.easyapi.com'
+        // window.location.href =
+        //   'https://team.easyapi.com/new?from=https://sms.easyapi.com'
       } else {
         Message.error(error.response.data.message)
       }
