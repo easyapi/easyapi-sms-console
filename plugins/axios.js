38c3116476813ca352158181bab8323df5ff0142
chenyun123 <1537299563@qq.com>
2021-08-03 10:43:27 +0800

    添加项目标题

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,9 +1,8 @@
 import Cookies from 'js-cookie'
-
-export default function ({$axios, redirect}) {
+import { Message } from 'element-ui'
+export default function ({ $axios, redirect }) {
   $axios.onRequest((config) => {
-    // let token = Cookies.get('Authorization')
-    let token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ6aGFuZzIwMDg0QDEyNi5jb20iLCJhdXRoIjoiUk9MRV9BRE1JTiIsImV4cCI6MTYzMDQ4MDA4NX0.V8OK1zKzx9GJCc8_X8IMeabdDZYXL6_VX3h3z43LfUHkQt4v0wQoAz9Vimr1jCf88oWguEnCOsYG02snh1FoxA'
+    let token = Cookies.get('Authorization')
     if (token) {
       config.headers.Authorization = 'Bearer ' + token
     }
@@ -12,11 +11,13 @@ export default function ({$axios, redirect}) {
     const code = parseInt(error.response && error.response.status)
     if (code === 400) {
       if (error.response.data.code === -9) {
-        // window.location.href = 'https://account.easyapi.com/login?from=https://sms.easyapi.com'
+        window.location.href =
+          'https://account.easyapi.com/login?from=https://sms.easyapi.com'
       } else if (error.response.data.code === -8) {
-        // window.location.href = 'https://team.easyapi.com/new?from=https://sms.easyapi.com'
+        window.location.href =
+          'https://team.easyapi.com/new?from=https://sms.easyapi.com'
       } else {
-        this.$message.warning(error.response.data.message)
+        Message.error(error.response.data.message)
       }
     }
   })
