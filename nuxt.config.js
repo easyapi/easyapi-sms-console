c5eaa31b858ad73774eb606a246385eb0cfae852
leida <zhang20084@126.com>
2021-08-16 10:37:13 +0800

    fix:合并

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,58 +3,62 @@ module.exports = {
    ** Headers of the page
    */
   head: {
-    title: '{{ name }} - EasyAPI服务市场',
+    title: '{{ name }} - EasyAPI短信服务',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       {
         hid: 'description',
         name: 'description',
-        content: '{{escape description }}',
-      },
+        content: '{{escape description }}'
+      }
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
-    script: [],
+    script: []
   },
   css: [
     '@/assets/scss/common.scss',
     '@/assets/scss/element-variables.scss',
     'quill/dist/quill.snow.css',
     'quill/dist/quill.bubble.css',
-    'quill/dist/quill.core.css',
+    'quill/dist/quill.core.css'
   ],
   plugins: [
     '@/plugins/element-ui',
     '@/plugins/axios',
     '@/plugins/echarts',
-    { src: '@/plugins/common.js', ssr: false },
+    { src: '@/plugins/common.js', ssr: false }
   ],
   /*
    ** Customize the progress bar color
    */
   modules: ['@nuxtjs/axios', '@nuxtjs/style-resources'],
+  router: {
+    base: '/console/'
+  },
   axios: {},
   loading: {
     color: '#18c1d6',
-    failedColor: '#18c1d6',
+    failedColor: '#18c1d6'
   },
   styleResources: {
-    scss: './assets/scss/variables.scss',
+    scss: './assets/scss/variables.scss'
   },
   /*
    ** Build configuration
    */
   build: {
     transpile: [/^element-ui/, 'vue-quill-editor'],
-    extend(config, ctx) {},
+    extend(config, ctx) {
+    },
     maxChunkSize: 300000, // 单个包最大尺寸
-    extractCSS: true, // 单独提取CSS
+    extractCSS: true // 单独提取CSS
   },
   server: {
-    port: 7777, // default: 3000
+    port: 3000
   },
   env: {
     baseUrl: 'https://account-api.easyapi.com',
-    serviceUrl: 'https://api.easyapi.com',
-  },
+    serviceUrl: 'https://api.easyapi.com'
+  }
 }
