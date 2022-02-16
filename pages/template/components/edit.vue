32fae803927e16e93a77219f7b10b21c9657b1ad
shdboom <928421021@qq.com>
2022-02-16 15:44:10 +0800

    签名优化

--- pages/template/components/edit.vue
+++ pages/template/components/edit.vue
@@ -4,25 +4,27 @@
     :title='title'
     :visible.sync='dialogVisible'
     width='30%'>
-    <el-form ref='childForm' :model='childForm' label-width='80px'>
-      <el-form-item label='模板标题'>
+    <el-form ref='childForm' :rules="rules" :model='childForm' label-width='80px'>
+      <el-form-item label='模板标题' prop="title">
         <el-input v-model='childForm.title' placeholder='请输入标题'></el-input>
       </el-form-item>
       <el-form-item label='模板正文'>
         <el-input type='textarea' :rows='8' placeholder='请输入短信内容' v-model='childForm.content'></el-input>
       </el-form-item>
-      <el-form-item>
+      <el-form-item label="签名" prop="signature">
         <div class='dynamicTags'>
           <el-select
             v-model='childForm.signature'
             placeholder='请选择'
+            id="aaa"
             clearable
             filterable
             @blur='selectBlur'
+            @focus="focus"
             @clear='selectClear'
             @change='selectChange'
           >
-            <el-option v-for='(item,index) in options' :key='index' :label='item.label' :value='item.value' />
+            <el-option v-for='(item,index) in options' :key='index' :label='item.label' :value='item.value'/>
           </el-select>
           <el-tag :key='tag' v-for='tag in dynamicTags' closable :disable-transitions='false' @close='handleClose(tag)'>
             {{ tag }}
@@ -45,109 +47,134 @@
 </template>
 
 <script>
-import { createSmsTemplate, updateSmsTemplate, getSmsSignatureList } from '../../../api/sms-template'
+  import {createSmsTemplate, updateSmsTemplate, getSmsSignatureList} from '../../../api/sms-template'
 
-export default {
-  name: 'addTemplate',
-  data() {
-    return {
-      dialogVisible: false,
-      title: '',
-      smsTemplateId: '',
-      childForm: {
+  export default {
+    name: 'addTemplate',
+    data() {
+      return {
+        dialogVisible: false,
         title: '',
-        content: '',
-        signature: ''
-      },
-      options: [],
-      dynamicTags: ['标签一']
-    }
-  },
-  mounted() {
-
-  },
-  computed: {},
-  methods: {
-    //获取签名列表
-    getSmsSignatureList() {
-      let params = {}
-      getSmsSignatureList(params, this).then(res => {
-      })
-    },
-    //移除标签
-    handleClose(tag) {
-      this.dynamicTags.splice(this.dynamicTags.indexOf(tag), 1)
-    },
-    confirm(formName) {
-      this.$refs[formName].validate(valid => {
-        if (valid) {
-          let data = { ...this.childForm }
-          if (this.title === '创建新模板') {
-            createSmsTemplate(data, this).then(res => {
-              if (res.data.code === 1) {
-                this.$message.success('添加成功')
-                this.$parent.getSmsTemplateList()
-                this.dialogVisible = false
-                this.$refs[formName].resetFields()
-              }
-            })
-          } else if (this.title === '编辑模板') {
-            updateSmsTemplate(this.smsTemplateId, data, this).then(res => {
-              if (res.data.code === 1) {
-                this.$message.success('修改成功')
-                this.$parent.getSmsTemplateList()
-                this.dialogVisible = false
-                this.$refs[formName].resetFields()
-              }
-            })
-          }
-        }
-      })
-    },
-    selectBlur(e) {
-      // 意见类型
-      if (e.target.value !== '') {
-        this.value = e.target.value
-        let obj = {}
-        obj.value = e.target.value
-        obj.label = e.target.value
-        this.options.push(obj)
-        this.$forceUpdate()   // 强制更新
+        smsTemplateId: '',
+        childForm: {
+          title: '',
+          content: '',
+          signature: ''
+        },
+        rules: {
+          signature: [
+            {required: true, message: '请选择活动区域', trigger: 'change'}
+          ]
+        },
+        options: [],
+        dynamicTags: ['标签一']
       }
     },
-    selectClear() {
-      this.value = ''
-      this.$forceUpdate()
+    mounted() {
+
     },
-    selectChange(val) {
-      this.value = val
-      this.$forceUpdate()
+    watch: {
+      dialogVisible(val) {
+        if (val) {
+          let input = document.querySelector(`#aaa`);
+          console.log(input)
+          if (input) {
+            input.setAttribute("maxLength", 13);
+            input.setAttribute("minLength", 2);
+          }
+        }
+      }
+    },
+    computed: {},
+    methods: {
+      //获取签名列表
+      getSmsSignatureList() {
+        let params = {}
+        getSmsSignatureList(params, this).then(res => {
+
+        })
+      },
+      //移除标签
+      handleClose(tag) {
+        this.dynamicTags.splice(this.dynamicTags.indexOf(tag), 1)
+      },
+      confirm(formName) {
+        this.$refs[formName].validate(valid => {
+          if (valid) {
+            let data = {...this.childForm}
+            if (this.title === '创建新模板') {
+              createSmsTemplate(data, this).then(res => {
+                if (res.data.code === 1) {
+                  this.$message.success('添加成功')
+                  this.$parent.getSmsTemplateList()
+                  this.dialogVisible = false
+                  this.$refs[formName].resetFields()
+                }
+              })
+            } else if (this.title === '编辑模板') {
+              updateSmsTemplate(this.smsTemplateId, data, this).then(res => {
+                if (res.data.code === 1) {
+                  this.$message.success('修改成功')
+                  this.$parent.getSmsTemplateList()
+                  this.dialogVisible = false
+                  this.$refs[formName].resetFields()
+                }
+              })
+            }
+          }
+        })
+      },
+      focus() {
+        let input = document.querySelector(`#aaa`);
+        if (input) {
+          input.setAttribute("maxLength", 13);
+          input.setAttribute("minLength", 2);
+        }
+      },
+      selectBlur(e) {
+        // 意见类型
+        if (e.target.value !== '') {
+          this.value = "【" + e.target.value + "】"
+          let obj = {}
+          obj.value = this.value
+          obj.label = this.value
+          this.options.push(obj)
+          this.$forceUpdate()   // 强制更新
+        }
+      },
+      selectClear() {
+        this.value = ''
+        this.$forceUpdate()
+      },
+      selectChange(val) {
+        this.value = val
+        this.$forceUpdate()
+      }
     }
   }
-}
 </script>
 
 <style lang='scss'>
-.el-dialog__header {
-  border-bottom: 1px solid #DCDFE6;
-}
+  .el-dialog__header {
+    border-bottom: 1px solid #DCDFE6;
+  }
 
-.template-content {
-  margin-bottom: 10px;
-}
+  .template-content {
+    margin-bottom: 10px;
+  }
 
-.dynamicTags {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-}
+  .dynamicTags {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
 
-.doc {
-  color: #18c1d6;
-  cursor: pointer;
-}
+  .doc {
+    color: #18c1d6;
+    cursor: pointer;
+  }
 
-.doc:hover {
-  text-decoration: underline;
-}
+  .doc:hover {
+    text-decoration: underline;
+  }
 </style>
--- plugins/axios.js
+++ plugins/axios.js
@@ -1,23 +1,23 @@
 import Cookies from 'js-cookie'
-import { Message } from 'element-ui'
+import {Message} from 'element-ui'
 
-export default function({ $axios, redirect }) {
+export default function ({$axios, redirect}) {
   $axios.onRequest((config) => {
-    let token = Cookies.get('fpAuthToken')
+    let token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ6aGFuZzIwMDg0QDEyNi5jb20iLCJhdXRoIjoiUk9MRV9BRE1JTiIsImV4cCI6MTY0NzU4NTg1OH0.2us0H53UW7sywer8LklxdIWTKJpvx2ReC6IThon51up5kX62sZ49bWWrZDRoXn0H6Ux1mmYiQ7mmXBmT7M95UQ"
     if (token) {
-      config.headers.fpAuthToken = 'Bearer ' + token
+      config.headers.Authorization = 'Bearer ' + token
     }
   })
-  $axios.onError((error) => {
-    const code = parseInt(error.response && error.response.status)
-    if (code === 400) {
-      if (error.response.data.code === -9) {
-        window.location.href = 'https://account.easyapi.com/login?from=https://sms.easyapi.com'
-      } else if (error.response.data.code === -8) {
-        window.location.href = 'https://team.easyapi.com/new?from=https://sms.easyapi.com'
-      } else {
-        Message.error(error.response.data.message)
-      }
-    }
-  })
+  // $axios.onError((error) => {
+  //   const code = parseInt(error.response && error.response.status)
+  //   if (code === 400) {
+  //     if (error.response.data.code === -9) {
+  //       window.location.href = 'https://account.easyapi.com/login?from=https://sms.easyapi.com'
+  //     } else if (error.response.data.code === -8) {
+  //       window.location.href = 'https://team.easyapi.com/new?from=https://sms.easyapi.com'
+  //     } else {
+  //       Message.error(error.response.data.message)
+  //     }
+  //   }
+  // })
 }
